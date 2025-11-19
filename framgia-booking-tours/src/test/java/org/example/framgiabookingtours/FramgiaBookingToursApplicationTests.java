c1ceced4605c2479e273cd19d50ccb734a0d12c9
ywhnaht <hothanhhuy24062004@gmail.com>
2025-11-19 13:49:21 +0700

    initiate spring boot project

---
+++ framgia-booking-tours/src/test/java/org/example/framgiabookingtours/FramgiaBookingToursApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.framgiabookingtours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FramgiaBookingToursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
